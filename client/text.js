1fbbbbe124b72057f77c7fb3920f12b32c485758
ytyeoh <ytyeoh@me.com>
2019-11-12 16:14:35 +0800

    change text and fix layout scroll

---
+++ client/text.js
@@ -0,0 +1,89 @@
+// header
+var brand = "Logisticsl"
+var menu = {
+  menu1:'home', menu2: 'works', menu3: 'process', menu4:'services'
+}
+
+// background images
+var background1 = "hero_4.jpg"
+var background2 = "hero_4.jpg"
+// section1
+var caption1a = 'Welcome To 4 <span class="fa fa-star text-primary"></span> Hotel'
+var captionH1 = 'Great Place to Hang around'
+// var welcomeImageR1 = 'person_1.jpg'
+var welcomeImageS1 = 'person_1.jpg'
+var captionH2a = 'We Value More on Quality and not in Quantity.!!!!'
+var caption1b = 'At Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.'
+var welcomeImageR1 = 'about_1.jpg'
+// section2 room
+var captionH2b = 'Rooms &amp; Suites'
+var caption2a= "awsome room with clean service"
+var rooms = {
+  room1: {
+    image: "img_1.jpg", type: 'vivip Room double', price: 'RM99999 / per night', facilities:{
+        Parking: 'Yes', Internet: 'Yes', Kitchen: 'Yes', Bedroom: '2', Bathroom: '1', 'Living Area': 'Yes', Amenities: 'fan, iron, clothes rack', Pets: 'not allow', Accessibility: 'Entire unit located on ground floor', Outdoors: 'BBQ facilities', Miscellaneous: 'Air conditioning, Non-smoking throughout, Family rooms'
+    }
+  },
+  room2: {
+    image: "img_2.jpg", type: 'Double Room', price: 'RM120 / per night', facilities:{
+        Parking: 'Yes', Internet: 'Yes', Kitchen: 'Yes', Bedroom: '2', Bathroom: '1', 'Living Area': 'Yes', Amenities: 'fan, iron, clothes rack', Pets: 'not allow', Accessibility: 'Entire unit located on ground floor', Outdoors: 'BBQ facilities', Miscellaneous: 'Air conditioning, Non-smoking throughout, Family rooms'
+    }
+  },
+  room3:{
+    image: "img_3.jpg", type: 'Family Room', price: 'RM250 / per night', facilities:{
+        Parking: 'Yes', Internet: 'Yes', Kitchen: 'Yes', Bedroom: '2', Bathroom: '1', 'Living Area': 'Yes', Amenities: 'fan, iron, clothes rack', Pets: 'not allow', Accessibility: 'Entire unit located on ground floor', Outdoors: 'BBQ facilities', Miscellaneous: 'Air conditioning, Non-smoking throughout, Family rooms'
+    }
+  }
+}
+
+// section3
+var caption3a = 'Hotel provice gym, swiiming pool'
+var galeryImages = {
+  1: 'slider-1.jpg', 2: 'slider-2.jpg', 3: 'slider-3.jpg', 4: 'slider-4.jpg', 5: 'slider-5.jpg'
+}
+
+// section4
+var captionH2d = "Attrations"
+var caption4a = 'Check out attration in Penang and enjoy it.'
+var attrations = {
+  1:{
+    title: "Penang Hill", image: 'img_1.jpg', desc: 'Penang famous attration'
+  },
+  2:{
+    title: "Kek Lok Si", image: 'img_2.jpg', desc: 'BIggest buddhist templa in SEA'
+  },
+  3:{
+    title: "Clan Jetty", image: 'img_3.jpg', desc: 'Residents on the sea'
+  }
+}
+
+// section5
+var tours = {
+  1:{
+    title: "Penang Hill", image: 'img_1.jpg', desc: 'Penang famous attration', url: 'http://www.kikia.co'
+  },
+  2:{
+    title: "Kek Lok Si", image: 'img_2.jpg', desc: 'BIggest buddhist templa in SEA', url: 'http://www.kikia.co'
+  },
+  3:{
+    title: "Clan Jetty", image: 'img_3.jpg', desc: 'Residents on the sea', url: 'http://www.kikia.co'
+  },
+  4:{
+    title: "Penang Hill", image: 'img_1.jpg', desc: 'Penang famous attration', url: 'http://www.kikia.co'
+  },
+  5:{
+    title: "Kek Lok Si", image: 'img_2.jpg', desc: 'BIggest buddhist templa in SEA', url: 'http://www.kikia.co'
+  },
+  6:{
+    title: "Clan Jetty", image: 'img_3.jpg', desc: 'Residents on the sea', url: 'http://www.kikia.co'
+  }
+}
+
+// section6
+var custId = 'ytyeoh@me.com'
+var address1 = '1,jalan satu, '
+var address2 = "Georgetown, 11800 Penang."
+var mobile = "0164286173"
+
+// footer
+var facebookUrl = 'https://www.facebook.com/kikiatour'
---
+++ js/paper.js
@@ -0,0 +1,85 @@
+document.getElementById('brand').innerHTML = brand;
+
+for (x in menu) {
+  document.getElementById(x).innerHTML = menu[x];
+  document.getElementById(x).href = "#"+menu[x];
+  document.getElementById(x).classList.add('menuLink');
+  var a = document.getElementById('a-'+x)
+  if (a!=null){
+    a.id = menu[x];
+  };
+  var b = document.getElementById('b-'+x)
+  if (b!=null){
+    b.innerHTML = menu[x];
+  };
+  document.getElementById('f'+x).innerHTML = menu[x];
+  document.getElementById('f'+x).href = "#"+menu[x];
+}
+document.getElementById('caption1a').innerHTML = caption1a;
+document.getElementById('captionH1').innerHTML = captionH1;
+document.getElementById('welcomeImageS1').src = 'client/images/' + welcomeImageS1;
+document.getElementById('welcomeImageR1').src = 'client/images/' + welcomeImageR1;
+document.getElementById('captionH2a').innerHTML = captionH2a;
+document.getElementById('caption1b').innerHTML = caption1b;
+document.getElementById('background1').style.backgroundImage = "url('client/images/" + background1 + "')";
+document.getElementById('background2').style.backgroundImage = "url('client/images/" + background2 + "')";
+
+// section2
+document.getElementById('captionH2b').innerHTML = captionH2b;
+document.getElementById('caption2a').innerHTML = caption2a;
+
+var y;
+for (y in rooms){
+  var divtest = document.createElement("div");
+  divtest.classList.add('col-lg-4', 'col-md-6', 'aos-init');
+  divtest.dataset.aos = "fade-up";
+  divtest.dataset.aos.delay = attrations[y] + '00';
+  divtest.innerHTML = '<figure class="img-wrap"><img src="client/images/' + rooms[y]["image"]+ '" alt="Image placeholder" class="img-fluid"></figure><div class="p-3 text-center room-info"><h2>' + rooms[y]["title"] + '</h2><span class="text-uppercase letter-spacing-1">' + rooms[y]["price"]+ '</span></div><div class="text-center"><button type="button" class="btn btn-info" data-toggle="modal" data-target="#my' + y + '">More</button></div>';
+  document.getElementById('roomPackage').appendChild(divtest);
+  document.getElementById('modalclass').innerHTML += '<div id="my' + y + '" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h4 class="modal-title">' + y + '</h4><button type="button" class="close" data-dismiss="modal">&times;</button></div><div class="modal-body"><p><img src="client/images/' + rooms[y]["image"]+ '" alt="Image placeholder" class="img-fluid"></p><div class="row" id="modal'+ y +'"></div></div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Back</button></div></div></div></div>';
+  for (f in rooms[y]["facilities"]) {
+
+    document.getElementById('modal'+y).innerHTML += '<p class="col-md-12 col-lg-6"><b>'+ f + '</b> : ' + rooms[y]["facilities"][f] + '</p>'
+  }
+}
+
+document.getElementById('caption3a').innerHTML = caption3a;
+for (x in galeryImages){
+  var divtest = document.createElement("div");
+  divtest.innerHTML = '<img src="client/images/' + galeryImages[x]+ '" alt="Image placeholder" class="img-fluid">';
+  document.getElementById('carousel').appendChild(divtest);
+}
+
+document.getElementById('captionH2d').innerHTML = captionH2d;
+document.getElementById('caption4a').innerHTML = caption4a;
+for (y in attrations){
+  var divtest = document.createElement("div");
+  divtest.classList.add('col-lg-4', 'col-md-6', 'col-sm-6', 'col-12', 'post', 'aos-init');
+  divtest.dataset.aos = "fade-up";
+  divtest.dataset.aos.delay = attrations[y] + '00';
+  divtest.innerHTML = '<div class="media media-custom d-block mb-4 h-100"><img src="client/images/' + attrations[y]['image'] + '" alt="Image placeholder" class="img-fluid"><div class="media-body"><h2 class="mt-0 mb-3">' + attrations[y]['title'] + '</h2><p>' + attrations[y]['desc'] + '</p></div></div>';
+  document.getElementById('media').appendChild(divtest);
+}
+
+for (y in tours){
+  var divtest = document.createElement("div");
+  divtest.classList.add('owl-item');
+  divtest.innerHTML = '<a href="'+ tours[y]["url"] +'"><div class="testimonial text-center slider-item"><img src="client/images/'+ tours[y]["image"]+'" alt="Image placeholder" class="mx-auto"></div><blockquote class="margin0"><h3>'+ tours[y]['title']+'</h3><p>' + tours[y]['desc'] + '</p></blockquote></a>';
+  document.getElementById('tours').appendChild(divtest);
+}
+
+
+// section6
+document.getElementById('list_custId').value = custId
+document.getElementById('list_address').value = address1 + address2
+document.getElementById('address').innerHTML = address1 + '<br>' + address2
+document.getElementById('mobile').innerHTML = mobile
+document.getElementById('whatsapp').href = "https://wa.me/6"+mobile
+document.getElementById('url').value = window.location.href 
+
+
+document.getElementById('fAddress').innerHTML = address1 + '<br>' + address2
+document.getElementById('fMobile').innerHTML = mobile
+
+document.getElementById('facebook').href = facebookUrl
+
